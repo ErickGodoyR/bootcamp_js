f606a0895fd6a56b273cabd8a86ddbeaab5f50a8
erick godoy <erickalexis23@hotmail.com>
2024-08-24 00:24:43 -0400

    feat modificacion navbar

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 })
 
 
-    /**modal */
+/**modal */
 var modal = document.querySelector(".modal")
 var main = document.querySelector("#main")
 var btnAbrirModal = document.querySelector("#btn-modal")
@@ -28,7 +28,6 @@ var cerrarModal = () => {
 }
 
 btnCerrarModal.onclick = cerrarModal
-
 /** */
 
 
@@ -62,4 +61,38 @@ function validarDatos(){
         document.getElementById('btn_enviar').disabled = true;
     }
 }
+/** */
+
+
+/**opciones navbar */
+var btnSkill = document.querySelector("#btn-skill")
+var btnTrabajos = document.querySelector("#btn-trabajos")
+var btnHome = document.querySelector("#btn-home")
+
+var infoTrabajos = document.querySelector("#trabajos")
+var infoPresentacion = document.querySelector(".presentacion")
+var infoSkill = document.querySelector("#skill")
+
+var mostrarSkill = () => {
+    infoSkill.style.display = "block"
+    infoTrabajos.style.display = "none"
+    infoPresentacion.style.display = "none"
+}
+
+var mostrarHome = () => {
+    infoPresentacion.style.display = "block"
+    infoSkill.style.display = "none"
+    infoTrabajos.style.display = "none"
+}
+
+var mostrartrabajos = () => {
+    infoTrabajos.style.display = "block"
+    infoPresentacion.style.display = "none"
+    infoSkill.style.display = "none"
+}
+
+btnHome.onclick = mostrarHome
+btnSkill.onclick = mostrarSkill 
+btnTrabajos.onclick = mostrartrabajos
+
 /** */
